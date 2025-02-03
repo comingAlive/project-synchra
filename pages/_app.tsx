28016d77fabddad90fc2b497896027bcaba10b8d
Max <max@aiwon.org>
2025-02-03 10:11:54 +0300

    fix styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,36 @@
+import "@/styles/globals.css";
+import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from "next/app";
 
 import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useRouter } from "next/router";
 
-import { fontSans, fontMono } from "@/config/fonts";
-import "@/styles/globals.css";
+import { fontMono, fontSans } from "@/lib/fonts";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { type Locale, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { config } from "@/lib/wagmi";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const { locale } = useRouter() as { locale: Locale };
 
   return (
-    <HeroUIProvider navigate={router.push}>
-      <NextThemesProvider>
-        <Component {...pageProps} />
-      </NextThemesProvider>
-    </HeroUIProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale={locale}>
+          <HeroUIProvider navigate={router.push}>
+            <NextThemesProvider>
+              <Component {...pageProps} />
+            </NextThemesProvider>
+          </HeroUIProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
