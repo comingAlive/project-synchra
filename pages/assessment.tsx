f28475e93216604f0685847948acff28b71d7876
Max <max@aiwon.org>
2025-02-03 15:01:27 +0300

    add note

--- pages/assessment.tsx
+++ pages/assessment.tsx
@@ -214,8 +214,9 @@ const AssessmentPage = () => {
 
   return (
     <DefaultLayout>
-      <div className=" bg-blue-50 border rounded-xl flex items-center justify-center p-4">
-        <Card className="w-full max-w-lg">
+      <div className=" bg-blue-50 border rounded-xl flex flex-col items-center justify-center p-4">
+        <div className="text-2xl mb-8">Based on your <span className="underline decoration-dashed decoration-from-font underline-offset-8">average behavior from the past</span></div>
+        <Card className="w-full max-w-lg mb-8 mt-8">
           <CardHeader>
             <div className="flex justify-between items-center w-full">
               <h2 className="text-2xl font-bold">
